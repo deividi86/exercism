b2e97480d44904eff54675e7151853aa39117afa
Deividi Silva <deividi@gmail.com>
2016-01-28 00:35:31 -0200

    js word count and fetch

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,7 +1,20 @@
 var words = function() {};
 
 words = function(phrase) {
-  var space = phrase.replace(/ /g,'');
+  var sentenceArray = phrase.split(/[\s,]+/);
+  var result = [];
+
+  for(i = 0; i < sentenceArray.length; i++){
+    if(result.hasOwnProperty(sentenceArray[i])){
+      result[sentenceArray[i]] += 1;
+    } else {
+      result[sentenceArray[i]] =1;
+    }
+  }
+  delete result[""];
+
+  return result;
+
 };
 
 
